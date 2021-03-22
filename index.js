6f18234a024127d80b52f20e4dbacdfebd149fb5
Tyler <tylerxwebber@gmail.com>
2021-03-22 14:04:47 -0500

    Rough draft 1.0 (insert task functional)

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
-$.ajax({
-    type: 'GET',
-    url:'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=1',
-    datatype: 'json',
-    success: function (response, textStatus) {
-        console.log(response);
-    },
-    error: function(request, textStatus, errorMessage) {
-        console.log(errorMessage);
+$(document).ready(function(){
+    var getAndDisplayAllTasks = function () {
+      $.ajax({
+        type: 'GET',
+        url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=19',
+        dataType: 'json',
+        success: function (response, textStatus) {
+            $('#todo-list').empty();    
+          response.tasks.forEach(function (task) {
+            $('#todo-list').append('<p>' + task.content + '</p>');
+          })
+        },
+        error: function (request, textStatus, errorMessage) {
+          console.log(errorMessage);
+        }
+      });
     }
-});
-
+    
+    var createTask = function () {
+      $.ajax({
+        type: 'POST',
+        url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=19',
+        contentType: 'application/json',
+        dataType: 'json',
+        data: JSON.stringify({
+          task: {
+            content: $('#new-task-content').val()
+          }
+        }),
+        success: function (response, textStatus) {
+            $('#new-task-content').val('');
+          getAndDisplayAllTasks();
+        },
+        error: function (request, textStatus, errorMessage) {
+          console.log(errorMessage);
+        }
+      });  
+    }
+    
+    $('#create-task').on('submit', function (e) {
+      e.preventDefault();
+      createTask();
+    });
+    
+    getAndDisplayAllTasks();
+    
+  });
